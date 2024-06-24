2778e2f2bcf8df9b54efa5e5b36923e74fa7b416
Shivansh <shivansh0301@gmail.com>
2024-06-25 02:14:19 +0530

    delete old deployments

--- publish/deployed/local/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	deployment: require('./deployment.json'),
-	tribes: require('./tribes.json'),
-	rewards: require('./rewards.json'),
-	feeds: require('./feeds.json'),
-	offchainFeeds: require('./offchain-feeds.json'),
-	futuresMarkets: require('./futures-markets.json'),
-	perpsv2Markets: require('./perpsv2-markets.json'),
-};
